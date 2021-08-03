0827df8e79d2bef9af68437f090758d463933576
sunyonghua <yonghua.s@icloud.com>
2021-08-03 11:13:46 +0800

    ✨ feat(路由): 增加 base 处理

--- .umirc.ts
+++ .umirc.ts
@@ -6,6 +6,7 @@ export default defineConfig({
   logo: 'https://avatars.githubusercontent.com/u/62231931?s=200&v=4',
   outputPath: 'docs-dist',
   publicPath: '/ii-hooks/',
+  base: '/ii-hooks/',
   mode: 'site',
   navs: [
     null, // null 值代表保留约定式生成的导航，只做增量配置
